b93b413494656e5d9771681b1ac4f4eb3a29aa29
Dreamguts <cliff.lipp@gmail.com>
2016-06-13 22:07:13 +0100

    ran JS with lint

--- src/UBookable/UBookable.Umbraco/Scripts/all.js
+++ src/UBookable/UBookable.Umbraco/Scripts/all.js
@@ -1,107 +1,83 @@
-﻿var DEBUG = true;
+﻿var DEBUG = true
 if (!DEBUG) {
-    if (!window.console) window.console = {};
-    var methods = ["log", "debug", "warn", "info"];
-    for (var i = 0; i < methods.length; i++) {
-        console[methods[i]] = function () { };
-    }
+  if (!window.console) window.console = {}
+  var methods = ['log', 'debug', 'warn', 'info']
+  for (var i = 0; i < methods.length; i++) {
+    console[methods[i]] = function () {}
+  }
 }
 
-
-  (function(yourcode) {
-      yourcode(window.jQuery, window, document);
-
-  }(function($, window, document) {
-
-      $(function() {
-          $('.full-clndr').clndr({
-              render: function (data) {
-                  return Mustache.render($('#calendar-tmpl').html(), data);
-              },
-              clickEvents: {
-                  click: function (target) {
-                      console.log(target);
-                      var bookable = $(target.element).closest(".bookable");
-                      var id = bookable.attr("data-id");
-                      var startTime = $.parseJSON(bookable.attr("data-start-time"));
-                      var endTime = $.parseJSON(bookable.attr("data-end-time"));
-                      var period = bookable.attr("data-period");
-                      var duration = bookable.attr("data-duration");
-
-                      var startDateTimeUTC = target.date._d.setHours(startTime.hours, startTime.mins);
-                      var endDateTimeUTC = target.date._d.setHours(endTime.hours, endTime.mins);
-
-
-
-                      renderTimeslots(id, startDateTimeUTC, endDateTimeUTC, period, duration, target.date.format("dddd, MMMM Do YYYY"));
-
-                  }
-              }
-          });
-
-
-          $('.bookable').on('click', '.make-booking', function () {
-              var me = $(this);
-              var id = me.attr("data-id");
-              var start = new Date(me.attr("data-start-time"));
-              var end = new Date(me.attr("data-end-time"));
-              var name = me.prev(".name").val();
-              var period = me.attr("data-period");
-              var duration = me.attr("data-duration");
-              console.log("make booking: " + id + " " + start + " " + end + " " + name);
-
-              if (name != "" && name != undefined) {
-                  uBookable.SaveBookingAndBooker(id, start, end, name).done(function (booking) {
-                      var bookable = me.closest(".bookable");
-                      var slot = me.closest(".slot");
-                      var period = bookable.attr("data-period");
-                      var duration = bookable.attr("data-duration");
-                      var dayDate = moment(start).format("dddd, MMMM Do YYYY");
-
-                      var daystart = slot.attr("data-day-start");
-                      var dayend = slot.attr("data-day-end");
-
-                      renderTimeslots(id, daystart, dayend, period, duration, dayDate)
-                  })
-              }
-
-          })
-
-      });
-
-      function renderTimeslots(id, startDateTimeUTC, endDateTimeUTC, period, duration, dayDate)
-      {
-          console.log(startDateTimeUTC);
-          console.log(endDateTimeUTC);
-          uBookable.GetTimeSlot(id, startDateTimeUTC, endDateTimeUTC, period, duration).done(function (timeslots) {
-              for (var x = 0, length = timeslots.length; x < length; x++) {
-                  timeslots[x].DateStart = timeslots[x].StartTime.FixSerialisedDate();
-                  timeslots[x].DateEnd = timeslots[x].EndTime.FixSerialisedDate();
-                  timeslots[x].StartTime = moment(timeslots[x].StartTime.FixSerialisedDate()).format('HH:SS');
-                  timeslots[x].EndTime = moment(timeslots[x].EndTime.FixSerialisedDate()).format('HH:SS');
-
-              }
-              timeslots["Title"] = "Available slots for " + dayDate;
-              timeslots["NodeId"] = id;
-
-              timeslots["DayStart"] = startDateTimeUTC;
-              timeslots["DayEnd"] = endDateTimeUTC;
-
-              $("#day-bookings").html(Mustache.render($('#day-bookings-tmpl').html(), timeslots));
-          })
+(function (bookingpage) {
+  bookingpage(window.jQuery, window, document)
+}(function ($, window, document) {
+  $(function () {
+    $('.full-clndr').clndr({
+      render: function (data) {
+        return Mustache.render($('#calendar-tmpl').html(), data)
+      },
+      clickEvents: {
+        click: function (target) {
+          console.log(target)
+          var bookable = $(target.element).closest('.bookable')
+          var id = bookable.attr('data-id')
+          var startTime = $.parseJSON(bookable.attr('data-start-time'))
+          var endTime = $.parseJSON(bookable.attr('data-end-time'))
+          var period = bookable.attr('data-period')
+          var duration = bookable.attr('data-duration')
+          var startDateTimeUTC = target.date._d.setHours(startTime.hours, startTime.mins)
+          var endDateTimeUTC = target.date._d.setHours(endTime.hours, endTime.mins)
+          renderTimeslots(id, startDateTimeUTC, endDateTimeUTC, period, duration, target.date.format('dddd, MMMM Do YYYY'))
+        }
       }
+    })
 
+    $('.bookable').on('click', '.make-booking', function () {
+      var me = $(this)
+      var id = me.attr('data-id')
+      var start = new Date(me.attr('data-start-time'))
+      var end = new Date(me.attr('data-end-time'))
+      var name = me.prev('.name').val()
+      console.log('make booking: ' + id + ' ' + start + ' ' + end + ' ' + name)
 
+      if (name !== '' && name !== undefined) {
+        uBookable.SaveBookingAndBooker(id, start, end, name).done(function (booking) {
+          var bookable = me.closest('.bookable')
+          var slot = me.closest('.slot')
+          var period = bookable.attr('data-period')
+          var duration = bookable.attr('data-duration')
+          var dayDate = moment(start).format('dddd, MMMM Do YYYY')
+
+          var daystart = slot.attr('data-day-start')
+          var dayend = slot.attr('data-day-end')
+
+          renderTimeslots(id, daystart, dayend, period, duration, dayDate)
+        })
+      }
+    })
+  })
+
+  function renderTimeslots (id, startDateTimeUTC, endDateTimeUTC, period, duration, dayDate) {
+    console.log(startDateTimeUTC)
+    console.log(endDateTimeUTC)
+    uBookable.GetTimeSlot(id, startDateTimeUTC, endDateTimeUTC, period, duration).done(function (timeslots) {
+      for (var x = 0, length = timeslots.length; x < length; x++) {
+        timeslots[x].DateStart = FixSerialisedDate(timeslots[x].StartTime)
+        timeslots[x].DateEnd = FixSerialisedDate(timeslots[x].EndTime)
+        timeslots[x].StartTime = moment(FixSerialisedDate(timeslots[x].StartTime)).format('HH:SS')
+        timeslots[x].EndTime = moment(FixSerialisedDate(timeslots[x].EndTime)).format('HH:SS')
+      }
+      timeslots['Title'] = 'Available slots for ' + dayDate
+      timeslots['NodeId'] = id
+
+      timeslots['DayStart'] = startDateTimeUTC
+      timeslots['DayEnd'] = endDateTimeUTC
+
+      $('#day-bookings').html(Mustache.render($('#day-bookings-tmpl').html(), timeslots))
+    })
   }
-));
+}
+))
 
-
-  Date.prototype.withoutTime = function () {
-      var d = new Date(this);
-      d.setHours(0, 0, 0, 0, 0);
-      return d
-  }
-
-  String.prototype.FixSerialisedDate = function () {
-      return new Date(parseInt(this.replace("/Date(", "").replace(")/", ""), 10));
-  }
+var FixSerialisedDate = function (stringIn) {
+  return new Date(parseInt(stringIn.replace('/Date(', '').replace(')/', ''), 10))
+}
--- src/UBookable/UBookable.Umbraco/Scripts/ubookable/ubookable.js
+++ src/UBookable/UBookable.Umbraco/Scripts/ubookable/ubookable.js
@@ -1,85 +1,80 @@
 ﻿var uBookable = (function ($) {
+  function _saveBookingAndBooker (nodeid, startDate, endDate, bookerName) {
+    console.log('save booking and booker: ')
+    var testBooking = Booking(nodeid, '', startDate, endDate)
+    var testBooker = Booker('Lachlann')
+    var data = {}
+    data['booker'] = testBooker
+    data['booking'] = testBooking
+    return $.ajax({
+      url: '/umbraco/api/booking/addbookingandbooker',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8'
+    })
+  }
 
+  function _saveBooking (nodeid, bookerid, startDate, endDate) {
+    console.log('save booking')
+    var testBooking = Booking(nodeid, bookerid, startDate, endDate)
+    return $.ajax({
+      url: '/umbraco/api/booking/addbooking',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(testBooking),
+      contentType: 'application/json; charset=utf-8'
+    })
+  }
 
-    function _saveBookingAndBooker(nodeid, startDate, endDate, bookerName) {
-        console.log("save booking and booker: ");
-        var testBooking = Booking(nodeid, "",startDate, endDate);
-        var testBooker = Booker('Lachlann');
-        var data = {};
-        data["booker"] = testBooker;
-        data["booking"] = testBooking;
-        return $.ajax({
-            url: '/umbraco/api/booking/addbookingandbooker',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            contentType: 'application/json; charset=utf-8'
-        });
-    }
+  function _getTimeSlots (id, startdate, enddate, period, duration) {
+    var params = {}
+    params['start'] = startdate
+    params['end'] = enddate
+    params['Id'] = id
+    params['period'] = period
+    params['duration'] = duration
 
-    function _saveBooking(nodeid, bookerid, startDate, endDate) {
-        console.log("save booking");
-        var testBooking = Booking(nodeid, bookerid, startDate, endDate);
-        var testBooker = Booker('Lachlann')
-         return $.ajax({
-            url: '/umbraco/api/booking/addbooking',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(testBooking),
-            contentType: 'application/json; charset=utf-8'
-        });
-    }
+    return $.getJSON('/umbraco/api/booking/getdaybookingslots', params)
+  }
 
-    function _getTimeSlots(id, startdate, enddate, period, duration)
-    {
-        var params = {};
-        params["start"] = startdate;
-        params["end"] = enddate;
-        params["Id"] = id;
-        params["period"] = period;
-        params["duration"] = duration;
+  function _saveBooker (name, memberid) {
+    var memId = (memberid !== undefined) ? memberid : ''
+    var testBooker = Booker(name, memId)
+    console.log(testBooker)
+    return $.ajax({
+      url: '/umbraco/api/booking/addbooker',
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(testBooker),
+      contentType: 'application/json; charset=utf-8'
+    })
+  }
 
-        return $.getJSON('/umbraco/api/booking/getdaybookingslots', params)
-
-    }
-
-    function _saveBooker(name, memberid) {
-        var memId = (memberid !== undefined) ? memberid : '';
-        var testBooker = Booker(name, memId);
-        console.log(testBooker);
-        return $.ajax({
-            url: '/umbraco/api/booking/addbooker',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(testBooker),
-            contentType: 'application/json; charset=utf-8'
-        });
-    }
-
-    return {
-        SaveBooking: _saveBooking,
-        SaveBooker: _saveBooker,
-        SaveBookingAndBooker : _saveBookingAndBooker,
-        GetTimeSlot: _getTimeSlots
-    };
-})(jQuery);
+  return {
+    SaveBooking: _saveBooking,
+    SaveBooker: _saveBooker,
+    SaveBookingAndBooker: _saveBookingAndBooker,
+    GetTimeSlot: _getTimeSlots
+  }
+})(jQuery)
 
 var Booking = function (nodeId, bookerID, startDate, endDate, approved, cancelled) {
-    //Model for a user submitted answer
-    return {
-        'nodeId': nodeId,
-        'bookerID': bookerID,
-        'startDate': startDate,
-        'endDate': endDate,
-        'approved': approved,
-        'cancelled': cancelled,
-    };
-};
+  // Model for a user submitted answer
+  return {
+    'nodeId': nodeId,
+    'bookerID': bookerID,
+    'startDate': startDate,
+    'endDate': endDate,
+    'approved': approved,
+    'cancelled': cancelled
+  }
+}
 var Booker = function (Name, memberid) {
-    //Model for a user submitted answer
-    
-    return {
-        'name': Name,
-        'memberId': memberid
-    };
-};
+  // Model for a user submitted answer
+
+  return {
+    'name': Name,
+    'memberId': memberid
+  }
+}
